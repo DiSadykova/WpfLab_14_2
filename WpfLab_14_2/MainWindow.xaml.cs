0be904a5e77eb5a398377464a6b3259b81991f81
Диляра Садыкова <dilyara.mozhet.sadykova@gmail.com>
2022-01-18 23:23:24 +0300

    Добавьте файлы проекта.

---
+++ WpfLab_14_2/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfLab_14_2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Product> products;
+        public MainWindow()
+        {
+            InitializeComponent();
+            products = new List<Product>();
+            products.Add(new Product()
+            {
+                Name = "Хлеб",
+                Price = 43.4,
+                ImageSourse = "Data/eda.jpg",
+                Category = Categories.Food
+            });
+            products.Add(new Product()
+            {
+                Name = "Мясо",
+                Price = 43.4,
+                ImageSourse = "Data/eda.jpg",
+                Category = Categories.Food
+            });
+            products.Add(new Product()
+            {
+                Name = "Чайник",
+                Price = 43.4,
+                ImageSourse = "Data/bitovayatehnika.jpg",
+                Category = Categories.Food
+            });
+            products.Add(new Product()
+            {
+                Name = "Блендер",
+                Price = 43.4,
+                ImageSourse = "Data/bitovayatehnika.jpg",
+                Category = Categories.Food
+            });
+            lstBox.ItemsSource = products;
+        }
+    }
+}
---
+++ WpfLab_14_2/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfLab_14_2
+{
+    public enum Categories
+    {
+        Food,
+        DomesticAppliances
+    }
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string ImageSourse { get; set; }
+        public Categories Category { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
